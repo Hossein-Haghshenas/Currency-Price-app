e45eed83a211403b18cdd409b081797e1b231872
Hossein <haghshenas3c@gmail.com>
2022-03-18 23:16:28 +0330

    display data for currencies

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,36 @@ const getData = async (url) => {
   try {
     const get = await axios.get(url);
     const data = get.data;
-  } catch (error) {}
+    displayData(data);
+  } catch (error) {
+    console.log(`something went wrong with error${error}`);
+  }
 };
 getData(apiUrl);
+
+/* display data for currencies */
+
+const displayData = (data) => {
+  const container = document.querySelector("#currencys-container");
+
+  for (const items of data) {
+    /* currencie container */
+    const newItem = document.createElement("li");
+    newItem.classList.add("currency-item");
+    /* currencie img */
+    const newImage = document.createElement("img");
+    newImage.classList.add("currencys-img");
+    newImage.setAttribute("src", items.logo_url);
+    /* currencie details */
+    const nameText = document.createElement("span");
+    nameText.textContent = items.currency;
+    const priceText = document.createElement("span");
+
+    let price = Number(items.price);
+    price = price < 0.01 ? price.toFixed(7) : price.toFixed(2);
+    priceText.textContent = `${price} $`;
+
+    newItem.append(newImage, nameText, priceText);
+    container.appendChild(newItem);
+  }
+};
